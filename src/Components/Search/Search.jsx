21c7b6a03a83b97ae88443733bf1165fce2b9dcf
YakhyaevGadji <gadji180105@gmail.com>
2024-02-07 21:50:40 +0300

    add redux search

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,12 +1,27 @@
-import { useContext } from "react";
+import { useCallback, useContext, useState } from "react";
 import styles from "./Search.module.scss";
 import { SearchContext } from "../../App/App";
+import debounce from "lodash.debounce";
 
 const Search = () => {
-    const {searchValue, setSearchValue} = useContext(SearchContext);
+    const [value, setValue] = useState('');
+    const { setSearchValue } = useContext(SearchContext);
+
+
+    const updateSearchValue = useCallback(
+        debounce((str) => {
+            setSearchValue(str);
+        }, 750),
+        [],
+    );
+
+    const onChangeInput = (event) => {
+        setValue(event.target.value);
+        updateSearchValue(event.target.value);
+    }
 
     return (
-        <input onChange={(event) => setSearchValue(event.target.value)} defaultValue={searchValue} className={styles.root} placeholder="Search pizza"/>
+        <input onChange={onChangeInput} className={styles.root} value={value} placeholder="Search pizza"/>
     );
 }
  
