d09bad0269a82a519ce5fb7dab410bf4e60ff1d2
YakhyaevGadji <gadji180105@gmail.com>
2024-02-11 11:08:47 +0300

    test

--- src/Pages/Catalog.jsx
+++ src/Pages/Catalog.jsx
@@ -10,6 +10,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { setCategoryId } from '../redux/slices/filterSlice';
 import qs from 'qs';
 import { useNavigate } from "react-router";
+import { setItems } from "../redux/slices/pizzasSlice";
 
 const Catalog = () => {
     const dispatch = useDispatch();
@@ -17,10 +18,10 @@ const Catalog = () => {
 
     const categoriId = useSelector(state => state.filter.categoryId);
     const sort = useSelector(state => state.filter.sort.sortProperty);
+    const items = useSelector(state => state.pizza.items);
 
 
-    const {searchValue} = useContext(SearchContext);    
-    const [items, setItems] = useState([]);
+    const {searchValue} = useContext(SearchContext);   
     const [isLoading, setIsLoading] = useState(true);
     const [currentPage, setCurrentPage] = useState(1);
   
@@ -28,19 +29,30 @@ const Catalog = () => {
         dispatch(setCategoryId(id));
     };
 
-    useEffect(() => {
+    const fetchPizzas = async () => {
         setIsLoading(true);
 
         const order = sort.includes('-') ? 'asc' : 'desc';
         const serach = searchValue ? `&search=${searchValue}` : searchValue;
 
-        axios.get(`https://65ad515fadbd5aa31be090e6.mockapi.io/items?page=${currentPage}&limit=4&${categoriId > 0 ? `category=${categoriId}` : ''}&sortBy=${sort.replace('-', '')}&order=${order}${serach}`)
-        .then((res) => {
-            setItems(res.data);
+        try {
+            const res = await axios.get(
+                `https://65ad515fadbd5aa31be090e6.mockapi.io/items?page=${currentPage}&limit=4&${categoriId > 0 ? `category=${categoriId}` : ''}&sortBy=${sort.replace('-', '')}&order=${order}${serach}`
+            );
+            dispatch(setItems(res.data));
             setIsLoading(false);
-        });
-
+        } catch (error) {
+            console.log(error);
+            setIsLoading(false);
+        }
+    
         window.scrollTo(0, 0);
+    };
+
+    useEffect(() => {
+        if(window.location.search) {
+            fetchPizzas();
+        }
     }, [categoriId, sort, currentPage, searchValue]);
 
     useEffect(() => {
